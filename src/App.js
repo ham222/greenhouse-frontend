350c500b6a86a9085da617b4625d53a6c5aaace1
M0Rek <morek.090@gmail.com>
2023-04-17 08:47:25 +0200

    Setup basic routing with placeholder components.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Status from "./Status";
+import Timeline from "./Timeline";
 
 function App() {
   return (
     <>
-    <div className="App">
-      <Navbar/> 
-    </div>
-  </>);
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Status />}/>
+          <Route exact path="/timeline" element={<Timeline/>}/>
+        </Routes>
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Status.js
@@ -0,0 +1,8 @@
+export default function Status() {
+  return (
+    <>
+      Here will be displayed a data component showing latest data from the IoT
+      device.
+    </>
+  );
+}
