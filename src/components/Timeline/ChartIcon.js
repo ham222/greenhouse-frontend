fe6951f95189b3c5778226a30f700313afe4d084
M0Rek <morek.090@gmail.com>
2023-04-18 19:47:36 +0200

    Parameterize chart icon to customize icon and bg

--- src/components/Timeline/ChartIcon.js
+++ src/components/Timeline/ChartIcon.js
@@ -1,9 +1,8 @@
-import { WiThermometer } from "react-icons/wi";
 
-export default function ChartIcon() {
+export default function ChartIcon({bgColor, icon}) {
   return (
-    <div className="w-10 h-10 p-1 rounded-xl bg-[#FFDCB6] flex justify-center items-center ">
-      <WiThermometer className="h-full w-full" />
+    <div style={{backgroundColor:bgColor}} className="w-10 h-10 p-1 rounded-xl bg-[#FFDCB6] flex justify-center items-center ">
+      {icon}
     </div>
   );
 }
