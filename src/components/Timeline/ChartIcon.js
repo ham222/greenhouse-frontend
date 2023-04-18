01722c793417a2631d3a59e913178eb92a4d597f
M0Rek <morek.090@gmail.com>
2023-04-18 16:32:36 +0200

    Create Chart Icon

---
+++ src/components/Timeline/ChartIcon.js
@@ -0,0 +1,9 @@
+import { WiThermometer } from "react-icons/wi";
+
+export default function ChartIcon() {
+  return (
+    <div className="w-10 h-10 p-1 rounded-xl bg-[#FFDCB6] flex justify-center items-center ">
+      <WiThermometer className="h-full w-full" />
+    </div>
+  );
+}
