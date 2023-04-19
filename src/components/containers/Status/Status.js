fad38e6f96372d0b90017cb222a5f1315f129470
Davidek776 <kocurdavid776@gmail.com>
2023-04-19 09:46:26 +0200

    Add parameter values

--- src/components/containers/Status/Status.js
+++ src/components/containers/Status/Status.js
@@ -3,7 +3,12 @@ import CurrentValBox from "./CurrentValBox";
 export default function Status() {
   return (
     <>
-      <CurrentValBox></CurrentValBox>
+      <CurrentValBox
+        temperature={24.8}
+        datetime={new Date().toLocaleTimeString()}
+        humidity={37}
+        co2={22.5}
+      ></CurrentValBox>
     </>
   );
 }
