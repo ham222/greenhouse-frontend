b878a3f12b40a1c15a35ae19f06c750d3c614d26
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2023-04-24 18:54:16 +0200

    Use MeasurementService in Status.tsx

--- src/components/containers/Status/Status.tsx
+++ src/components/containers/Status/Status.tsx
@@ -1,32 +1,28 @@
 import CurrentValBox from "./CurrentValBox";
 import { useEffect, useState } from "react";
-import {
-  getTemperature,
-  getHumidity,
-  getCo2,
-} from "../../../services/thcService";
+import MeasurementService from "src/services/MeasurementService";
 
 export default function Status() {
   const [temperature, setTemperature] = useState(null);
   const [humidity, setHumidity] = useState(null);
   const [co2, setCo2] = useState(null);
   const [date, setDate] = useState(null);
-
   useEffect(() => {
     let mounted = true;
-    getTemperature().then((currentTemperature) => {
+    const measurementService = new MeasurementService();
+    measurementService.getTemperature().then((currentTemperature) => {
       if (mounted) {
         setTemperature(currentTemperature.value);
       }
     });
 
-    getHumidity().then((currentHumidity) => {
+    measurementService.getHumidity().then((currentHumidity) => {
       if (mounted) {
         setHumidity(currentHumidity.value);
       }
     });
 
-    getCo2().then((currentCo2) => {
+    measurementService.getCo2().then((currentCo2) => {
       if (mounted) {
         setCo2(currentCo2.value);
         setDate(currentCo2.timestamp);
