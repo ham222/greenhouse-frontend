eb466521b6bce18d619a97001527a96f7d1cb1db
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2023-04-25 13:47:04 +0200

    Update status to use current data method

--- src/components/containers/Status/Status.tsx
+++ src/components/containers/Status/Status.tsx
@@ -3,26 +3,26 @@ import { useEffect, useState } from "react";
 import MeasurementService from "src/services/MeasurementService";
 
 export default function Status() {
-  const [temperature, setTemperature] = useState(null);
-  const [humidity, setHumidity] = useState(null);
-  const [co2, setCo2] = useState(null);
-  const [date, setDate] = useState(null);
+  const [temperature, setTemperature] = useState<number | null>(null);
+  const [humidity, setHumidity] = useState<number | null>(null);
+  const [co2, setCo2] = useState<number | null>(null);
+  const [date, setDate] = useState<number | null>(null);
   useEffect(() => {
     let mounted = true;
     const measurementService = new MeasurementService();
-    measurementService.getTemperature().then((currentTemperature) => {
+    measurementService.getCurrentTemperature().then((currentTemperature) => {
       if (mounted) {
         setTemperature(currentTemperature[0].value);
       }
     });
 
-    measurementService.getHumidity().then((currentHumidity) => {
+    measurementService.getCurrentHumidity().then((currentHumidity) => {
       if (mounted) {
         setHumidity(currentHumidity[0].value);
       }
     });
 
-    measurementService.getCo2().then((currentCo2) => {
+    measurementService.getCurrentCo2().then((currentCo2) => {
       if (mounted) {
         setCo2(currentCo2[0].value);
         setDate(currentCo2[0].timestamp);
@@ -41,10 +41,10 @@ export default function Status() {
   return (
     <>
       <CurrentValBox
-        temperature={temperature ?? ""}
+        temperature={temperature?.toString() ?? ""}
         datetime={new Date(date ?? "").toLocaleString()}
-        humidity={humidity ?? ""}
-        co2={co2 ?? ""}
+        humidity={humidity?.toString() ?? ""}
+        co2={co2?.toString() ?? ""}
       ></CurrentValBox>
     </>
   );
