b4c95813d6fd0f74ab64aff63d323e7b34faa41d
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2023-04-25 13:46:09 +0200

    Update measurement service to accept timestamps

--- src/services/MeasurementService.ts
+++ src/services/MeasurementService.ts
@@ -1,12 +1,37 @@
 import axios from "axios";
+import { Measurement } from "src/domain/Measurement";
 
 class MeasurementService {
-  async getTemperature() {
+  async getCurrentTemperature(): Promise<Measurement[]> {
+    return this.getTemperature(undefined, undefined, true);
+  }
+
+  async getTemperature(
+    startTimestamp?: number,
+    endTimestamp?: number,
+    current: boolean = false
+  ): Promise<Measurement[]> {
     let temperatureList;
+
     try {
-      const response = await axios.get(
-        `http://localhost:3100/api/temperature?current`
-      );
+      var url = `http://localhost:3100/api/temperature`;
+      if (current) {
+        url += "?current";
+      } else {
+        if (startTimestamp !== undefined) {
+          url += `?startTimestamp=${startTimestamp}`;
+        }
+        if (endTimestamp !== undefined) {
+          if (startTimestamp !== undefined) {
+            url += "&";
+          } else {
+            url += "?";
+          }
+          url += `endTimestamp=${endTimestamp}`;
+        }
+      }
+      console.log(url);
+      const response = await axios.get(url);
       if (response.status !== 200) return [];
 
       temperatureList = response.data;
@@ -18,12 +43,37 @@ class MeasurementService {
     }
     return temperatureList;
   }
-  async getHumidity() {
+
+  async getCurrentHumidity(): Promise<Measurement[]> {
+    return this.getHumidity(undefined, undefined, true);
+  }
+
+  async getHumidity(
+    startTimestamp?: number,
+    endTimestamp?: number,
+    current: boolean = false
+  ): Promise<Measurement[]> {
     let temperatureList;
     try {
-      const response = await axios.get(
-        `http://localhost:3100/api/humidity?current`
-      );
+      var url = `http://localhost:3100/api/humidity`;
+      if (current) {
+        url += "?current";
+      } else {
+        if (startTimestamp !== undefined) {
+          url += `?startTimestamp=${startTimestamp}`;
+        }
+        if (endTimestamp !== undefined) {
+          if (startTimestamp !== undefined) {
+            url += "&";
+          } else {
+            url += "?";
+          }
+          url += `endTimestamp=${endTimestamp}`;
+        }
+      }
+      console.log(url);
+      const response = await axios.get(url);
+
       if (response.status !== 200) return [];
 
       temperatureList = response.data;
@@ -35,10 +85,36 @@ class MeasurementService {
     }
     return temperatureList;
   }
-  async getCo2() {
+
+  async getCurrentCo2(): Promise<Measurement[]> {
+    return this.getCo2(undefined, undefined, true);
+  }
+
+  async getCo2(
+    startTimestamp?: number,
+    endTimestamp?: number,
+    current: boolean = false
+  ): Promise<Measurement[]> {
     let temperatureList;
     try {
-      const response = await axios.get(`http://localhost:3100/api/co2?current`);
+      var url = `http://localhost:3100/api/co2`;
+      if (current) {
+        url += "?current";
+      } else {
+        if (startTimestamp !== undefined) {
+          url += `?startTimestamp=${startTimestamp}`;
+        }
+        if (endTimestamp !== undefined) {
+          if (startTimestamp !== undefined) {
+            url += "&";
+          } else {
+            url += "?";
+          }
+          url += `endTimestamp=${endTimestamp}`;
+        }
+      }
+      console.log(url);
+      const response = await axios.get(url);
       if (response.status !== 200) return [];
 
       temperatureList = response.data;
