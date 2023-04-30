3692fb584e833d2bdb19dabd032ea622793c8927
M0Rek <morek.090@gmail.com>
2023-04-30 21:42:31 +0200

    Create watering route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Status from "./components/containers/Status/Status";
 import Timeline from "./components/containers/Timeline/Timeline";
+import Watering from "./components/containers/Watering/Watering";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Status />} />
             <Route path="/timeline" element={<Timeline />} />
+            <Route path="/watering" element={<Watering />} />
           </Routes>
         </div>
         <Navbar />
---
+++ src/components/containers/Watering/Watering.tsx
@@ -0,0 +1,3 @@
+export default function Watering() {
+  return <div></div>;
+}
