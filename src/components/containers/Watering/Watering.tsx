71fcc43a50bc8a7567d4ba90cd90bd1a86f8f806
M0Rek <morek.090@gmail.com>
2023-04-30 23:36:16 +0200

    Create day picker for watering schedule

--- src/components/containers/Watering/Watering.tsx
+++ src/components/containers/Watering/Watering.tsx
@@ -1,3 +1,14 @@
+import DayPicker from "./DayPicker";
+
 export default function Watering() {
-  return <div></div>;
+
+    return (
+    <div className="m-3">
+      <div className="text-center my-8 font-bold">Watering Schedule</div>
+      <div>
+        <DayPicker/>
+        <div className="flex justify-end mt-1 mr-2">{">>"}</div>
+      </div>
+    </div>
+  );
 }
