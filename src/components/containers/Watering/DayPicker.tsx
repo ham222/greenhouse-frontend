2610aa21521ca026ec18282264304a7e8a288ec3
M0Rek <morek.090@gmail.com>
2023-05-01 14:46:53 +0200

    Show cursor pointer on hover

--- src/components/containers/Watering/DayPicker.tsx
+++ src/components/containers/Watering/DayPicker.tsx
@@ -14,7 +14,7 @@ export default function DayPicker() {
       <div className="overflow-hidden h-20">
         <div className="flex flex-shrink-0 overflow-x-scroll overflow-y-hidden h-21">
           {weekDays.map((day) => (
-            <div className="mr-4 font-semibold text-center w-14 flex justify-center items-center h-16 my-4 py-4 px-2 flex-shrink-0 rounded-lg bg-[#F2F4F5]">
+            <div className="mr-4 font-semibold cursor-pointer text-center w-14 flex justify-center items-center h-16 my-4 py-4 px-2 flex-shrink-0 rounded-lg bg-[#F2F4F5]">
               {day.substring(0, 3)}
             </div>
           ))}
