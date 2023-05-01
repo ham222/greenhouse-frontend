3a2591564bd0a155c9649bf9591278d01fca14ba
M0Rek <morek.090@gmail.com>
2023-05-01 09:33:31 +0200

    Add WaterToggle and WaterRuntime to Watering page

--- src/components/containers/Watering/Watering.tsx
+++ src/components/containers/Watering/Watering.tsx
@@ -1,13 +1,27 @@
+import IconButton from "src/components/UI/IconButton";
+import { IoIosAdd } from "react-icons/io";
 import DayPicker from "./DayPicker";
+import WaterToggle from "./WaterToggle";
+import WaterRuntime from "./WaterRuntime";
+import { DateTime } from "luxon";
 
 export default function Watering() {
-
-    return (
-    <div className="m-3">
-      <div className="text-center my-8 font-bold">Watering Schedule</div>
+  return (
+    <div className="m-3 flex flex-col">
+      <div className="text-center my-7 font-bold">Watering Schedule</div>
       <div>
-        <DayPicker/>
-        <div className="flex justify-end mt-1 mr-2">{">>"}</div>
+        <WaterToggle />
+        <DayPicker />
+      </div>
+      <div className="font-semibold mt-10 mb-2">Timeline</div>
+      <div>
+        <IconButton icon={<IoIosAdd className="text-white w-full h-full" />} />
+      </div>
+      <div className="space-y-4 mt-4">
+        <WaterRuntime
+          startTime={DateTime.now()}
+          endTime={DateTime.now().plus({ minutes: 15 })}
+        />
       </div>
     </div>
   );
