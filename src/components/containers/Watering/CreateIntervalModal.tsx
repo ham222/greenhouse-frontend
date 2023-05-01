36cafb2fb2154b924ea8c3beb5144f05f2798953
M0Rek <morek.090@gmail.com>
2023-05-01 14:49:24 +0200

    Center interval modal title

--- src/components/containers/Watering/CreateIntervalModal.tsx
+++ src/components/containers/Watering/CreateIntervalModal.tsx
@@ -43,7 +43,7 @@ export default function CreateIntervalModal({
                     <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                       <Dialog.Title
                         as="h3"
-                        className="text-base font-semibold leading-6 text-gray-900"
+                        className="text-base text-center font-semibold leading-6 text-gray-900"
                       >
                         New interval
                       </Dialog.Title>
