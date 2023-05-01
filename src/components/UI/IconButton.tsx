c5be9a54b10a0cf43d35a98e4ad1d8bace970ef7
M0Rek <morek.090@gmail.com>
2023-05-01 11:02:33 +0200

    Implement onClick prop functionality in IconButton

--- src/components/UI/IconButton.tsx
+++ src/components/UI/IconButton.tsx
@@ -1,13 +1,16 @@
 import { ButtonUnstyled } from "@mui/base";
 
 interface IconButtonProps {
-  onClick?: Function;
+  onClick: () => void;
   icon: React.ReactNode;
 }
 
 export default function IconButton({ onClick, icon }: IconButtonProps) {
   return (
-    <ButtonUnstyled className="w-10 h-10 rounded-lg bg-[#202329] hover:bg-[#393e49] duration-100 grid place-self-center">
+    <ButtonUnstyled
+      onClick={() => onClick()}
+      className="w-10 h-10 rounded-lg bg-[#202329] hover:bg-[#393e49] duration-100 grid place-self-center"
+    >
       <div className="w-full h-full text-white grid place-self-center">
         {icon}
       </div>
