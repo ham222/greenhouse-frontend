95069803cd25bfdcf343403484a635649c72ff86
M0Rek <morek.090@gmail.com>
2023-05-01 19:42:59 +0200

    Create MiniDay Picker for picking days of the week

---
+++ src/components/UI/MiniDayPicker.tsx
@@ -0,0 +1,26 @@
+import MiniDay from "./MiniDay";
+import { WeekDay } from "src/domain/WeekDay";
+import { DayPick } from "src/domain/DayPick";
+
+interface MiniDayPickerProps {
+  value: DayPick[];
+  updateValue: (day: WeekDay, value: boolean) => void;
+}
+
+export default function MiniDayPicker({
+  value,
+  updateValue,
+}: MiniDayPickerProps) {
+  return (
+    <div className="flex justify-center gap-4">
+      {value.map(({ day }) => (
+        <MiniDay
+          key={day}
+          value={value.find((dp) => dp.day === day)?.picked || false}
+          updateValue={(value: boolean) => updateValue(day, value)}
+          day={day}
+        />
+      ))}
+    </div>
+  );
+}
