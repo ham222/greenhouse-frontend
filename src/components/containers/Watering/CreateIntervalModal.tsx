613a3749b7f8a2c408cddbdb668f129328a9120f
M0Rek <morek.090@gmail.com>
2023-05-01 16:28:00 +0200

    Make modal reusable

--- src/components/containers/Watering/CreateIntervalModal.tsx
+++ src/components/containers/Watering/CreateIntervalModal.tsx
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import HourPicker from "src/components/UI/HourPicker";
+import Modal from "src/components/UI/Modal";
 
 interface IntervalModalProps {
   open: boolean;
@@ -12,68 +13,43 @@ export default function CreateIntervalModal({
   onClose,
 }: IntervalModalProps) {
   return (
-    <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={onClose}>
-        <Transition.Child
-          as={Fragment}
-          enter="ease-out duration-300"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-200"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-        </Transition.Child>
-
-        <div className="fixed inset-0 z-10 overflow-y-auto ">
-          <div className="flex min-h-full items-start justify-center text-center sm:items-center sm:p-0">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+    <Modal open={open} onClose={onClose}>
+      <div className="bg-white p-6">
+        <div className="flex  items-center justify-center sm:items-start">
+          <div className="mt-3 flex flex-col text-center sm:ml-4 sm:mt-0">
+            <Dialog.Title
+              as="h3"
+              className="text-xl text-center font-semibold leading-6 text-gray-900"
             >
-              <Dialog.Panel className="relative transform overflow-hidden max-sm:h-screen sm:rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 w-full sm:max-w-lg">
-                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                  <div className="flex items-center justify-center sm:items-start">
-                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                      <Dialog.Title
-                        as="h3"
-                        className="text-base text-center font-semibold leading-6 text-gray-900"
-                      >
-                        New interval
-                      </Dialog.Title>
-                      <div className="mt-2">
-                        <HourPicker/>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="px-4 py-3 flex  flex-row-reverse sm:px-6">
-                  <button
-                    type="button"
-                    className="inline-flex max-sm:basis-1/2 max-sm:mx-4 justify-center rounded-md bg-[#F2F4F5] px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 ml-3"
-                    onClick={() => onClose()}
-                  >
-                    Confirm
-                  </button>
-                  <button
-                    type="button"
-                    className="inline-flex max-sm:basis-1/2 max-sm:mx-4 justify-center rounded-md bg-[#F2F4F5] px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0"
-                    onClick={() => onClose()}
-                  >
-                    Cancel
-                  </button>
-                </div>
-              </Dialog.Panel>
-            </Transition.Child>
+              New interval
+            </Dialog.Title>
+            <div className="mt-20 sm:flex sm:justify-between">
+              <div>
+                <HourPicker />
+              </div>
+              <div className="mt-20 sm:ml-8 sm:mt-0">
+                <HourPicker />
+              </div>
+            </div>
           </div>
         </div>
-      </Dialog>
-    </Transition.Root>
+      </div>
+      <div className="px-4 py-3 flex  flex-row-reverse sm:px-6">
+        <button
+          type="button"
+          className="inline-flex max-sm:basis-1/2 max-sm:mx-4 justify-center rounded-md bg-[#F2F4F5] px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 ml-3"
+          onClick={() => onClose()}
+        >
+          Confirm
+        </button>
+        <button
+          type="button"
+          className="inline-flex max-sm:basis-1/2 max-sm:mx-4 justify-center rounded-md bg-[#F2F4F5] px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0"
+          onClick={() => onClose()}
+        >
+          Cancel
+        </button>
+      </div>
+    </Modal>
   );
 }
