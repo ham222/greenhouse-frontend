f302c5b5e5335950bb04a29ba4b0a18ff396b490
M0Rek <morek.090@gmail.com>
2023-05-02 08:51:34 +0200

    Modify watering page to use groupedIntervals

--- src/components/containers/Watering/Watering.tsx
+++ src/components/containers/Watering/Watering.tsx
@@ -5,44 +5,47 @@ import WaterRuntime from "./WaterRuntime";
 import { DateTime } from "luxon";
 import CreateIntervalModal from "./CreateIntervalModal";
 import { Fragment, useState } from "react";
-import { Interval } from "src/domain/Interval";
 import { Tab } from "@headlessui/react";
 import { WeekDays } from "src/domain/WeekDays";
+import { groupIntervals } from "src/utils/groupIntervals";
+import { GroupedIntervals } from "src/domain/GroupedIntervals";
 
 export default function Watering() {
   const [open, setOpen] = useState(false);
-  const [intervals, setIntervals] = useState<Interval[]>([
-    {
-      startTime: DateTime.fromObject({ hour: 5, minute: 0 }),
-      endTime: DateTime.fromObject({ hour: 6, minute: 0 }),
-      dayOfWeek: "Monday",
-    },
-    {
-      startTime: DateTime.fromObject({ hour: 5, minute: 0 }),
-      endTime: DateTime.fromObject({ hour: 6, minute: 0 }),
-      dayOfWeek: "Wednesday",
-    },
-    {
-      startTime: DateTime.fromObject({ hour: 5, minute: 0 }),
-      endTime: DateTime.fromObject({ hour: 6, minute: 0 }),
-      dayOfWeek: "Friday",
-    },
-    {
-      startTime: DateTime.fromObject({ hour: 15, minute: 0 }),
-      endTime: DateTime.fromObject({ hour: 15, minute: 30 }),
-      dayOfWeek: "Friday",
-    },
-    {
-      startTime: DateTime.fromObject({ hour: 16, minute: 0 }),
-      endTime: DateTime.fromObject({ hour: 16, minute: 30 }),
-      dayOfWeek: "Friday",
-    },
-    {
-      startTime: DateTime.fromObject({ hour: 17, minute: 0 }),
-      endTime: DateTime.fromObject({ hour: 17, minute: 30 }),
-      dayOfWeek: "Friday",
-    },
-  ]);
+  const [intervals, setIntervals] = useState<GroupedIntervals>(
+    groupIntervals([
+      {
+        startTime: DateTime.fromObject({ hour: 5, minute: 0 }),
+        endTime: DateTime.fromObject({ hour: 6, minute: 0 }),
+        dayOfWeek: "Monday",
+      },
+      {
+        startTime: DateTime.fromObject({ hour: 5, minute: 0 }),
+        endTime: DateTime.fromObject({ hour: 6, minute: 0 }),
+        dayOfWeek: "Wednesday",
+      },
+      {
+        startTime: DateTime.fromObject({ hour: 5, minute: 0 }),
+        endTime: DateTime.fromObject({ hour: 6, minute: 0 }),
+        dayOfWeek: "Friday",
+      },
+      {
+        startTime: DateTime.fromObject({ hour: 15, minute: 0 }),
+        endTime: DateTime.fromObject({ hour: 15, minute: 30 }),
+        dayOfWeek: "Friday",
+      },
+      {
+        startTime: DateTime.fromObject({ hour: 16, minute: 0 }),
+        endTime: DateTime.fromObject({ hour: 16, minute: 30 }),
+        dayOfWeek: "Friday",
+      },
+      {
+        startTime: DateTime.fromObject({ hour: 17, minute: 0 }),
+        endTime: DateTime.fromObject({ hour: 17, minute: 30 }),
+        dayOfWeek: "Friday",
+      },
+    ])
+  );
 
   return (
     <>
@@ -63,9 +66,7 @@ export default function Watering() {
                           "font-semibold focus:outline-0 cursor-pointer text-center w-14 flex justify-center items-center h-16 my-4 py-4 px-2 flex-shrink-0 rounded-lg",
                           selected
                             ? "bg-[#202329] text-white"
-                            : intervals.find(
-                                (interval) => interval.dayOfWeek === day
-                              )
+                            : intervals[day].length > 0
                             ? "bg-[#E6F5FB]"
                             : "bg-[#F2F4F5]",
                         ].join(" ")}
@@ -88,18 +89,14 @@ export default function Watering() {
           <Tab.Panels className="mt-4">
             {WeekDays.map((day) => (
               <Tab.Panel key={day}>
-                {intervals.map((interval, index) =>
-                  interval.dayOfWeek === day ? (
-                    <div key={index} className="mt-2">
-                      <WaterRuntime
-                        startTime={interval.startTime}
-                        endTime={interval.endTime}
-                      />
-                    </div>
-                  ) : (
-                    ""
-                  )
-                )}
+                {intervals[day].map((interval, index) => (
+                  <div key={index} className="mt-2">
+                    <WaterRuntime
+                      startTime={interval.startTime}
+                      endTime={interval.endTime}
+                    />
+                  </div>
+                ))}
               </Tab.Panel>
             ))}
           </Tab.Panels>
