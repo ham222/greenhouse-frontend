69b7f769c798a8a508836c4fadd3eb84098d033c
M0Rek <morek.090@gmail.com>
2023-05-02 09:08:06 +0200

    Pass toggle props in water toggle

--- src/components/containers/Watering/WaterToggle.tsx
+++ src/components/containers/Watering/WaterToggle.tsx
@@ -1,10 +1,12 @@
-import Toggle from "src/components/UI/Toggle";
+import Toggle, { ToggleProps } from "src/components/UI/Toggle";
 
-export default function WaterToggle() {
+interface WaterToggleProps extends ToggleProps {}
+
+export default function WaterToggle(props: WaterToggleProps) {
   return (
     <div className="bg-[#202329] rounded-lg text-white items-center font-semibold py-6 px-4 flex justify-between">
       <div>Manual Toggle</div>
-      <Toggle />
+      <Toggle {...props} />
     </div>
   );
 }
