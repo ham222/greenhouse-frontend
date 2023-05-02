cce07584039f022c4d754c6529c867d0f2bdc9c2
M0Rek <morek.090@gmail.com>
2023-05-02 09:07:38 +0200

    Update toggle to depend on external state

--- src/components/UI/Toggle.tsx
+++ src/components/UI/Toggle.tsx
@@ -1,21 +1,24 @@
 import { useState } from "react";
 import { Switch } from "@headlessui/react";
 
-export default function Toggle() {
-  const [enabled, setEnabled] = useState(true);
+export interface ToggleProps {
+  value: boolean;
+  updateValue: () => void;
+}
 
+export default function Toggle({ value, updateValue }: ToggleProps) {
   return (
     <Switch
-      checked={enabled}
-      onChange={setEnabled}
+      checked={value}
+      onChange={updateValue}
       className={`${
-        enabled ? "bg-[#BFFA00]" : "bg-stone-400"
+        value ? "bg-[#BFFA00]" : "bg-stone-400"
       } relative inline-flex h-6 w-11 items-center rounded-full`}
     >
       <span className="sr-only">Enable notifications</span>
       <span
         className={`${
-          enabled ? "translate-x-6" : "translate-x-1"
+          value ? "translate-x-6" : "translate-x-1"
         } inline-block h-4 w-4 transform rounded-full bg-[#202329] transition`}
       />
     </Switch>
