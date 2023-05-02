869aafa08d6de68ffccf1b3c5e53b7423285c2ef
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2023-05-02 17:29:52 +0200

    Update mock data

--- mocks/routes/schedule.js
+++ mocks/routes/schedule.js
@@ -5,18 +5,18 @@ let schedule = [
     dayOfWeek: 1,
   },
   {
-    startTime: "12:30:00",
-    endTime: "14:00:00",
+    startTime: "18:30:00",
+    endTime: "20:00:00",
     dayOfWeek: 2,
   },
   {
-    startTime: "12:30:00",
-    endTime: "14:00:00",
+    startTime: "12:15:00",
+    endTime: "14:20:00",
     dayOfWeek: 3,
   },
   {
-    startTime: "12:30:00",
-    endTime: "14:00:00",
+    startTime: "10:30:00",
+    endTime: "16:10:00",
     dayOfWeek: 4,
   },
 ];
