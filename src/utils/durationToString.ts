55d186a1bef8673dc3eaacb3f8b061e97dabceb0
M0Rek <morek.090@gmail.com>
2023-05-04 15:02:28 +0200

    Test and revise durationToString function

--- src/utils/durationToString.ts
+++ src/utils/durationToString.ts
@@ -2,7 +2,14 @@ import { Duration } from "luxon";
 
 export default function durationToString(duration: Duration): string {
   //Rescale is performed twice. Once to shift miliseconds to all needed units, second time to remove empty miliseconds attribute after rounding it down to 0
-  return duration.rescale().set({ milliseconds: 0 }).rescale().toHuman({
+
+  if (duration.as("milliseconds") <= 0) {
+    duration = Duration.fromObject({ seconds: 0 });
+  } else {
+    duration = duration.rescale().set({ milliseconds: 0 }).rescale();
+  }
+
+  return duration.toHuman({
     unitDisplay: "narrow",
     listStyle: "narrow",
     useGrouping: false,
