218b2f837172d9ddba95168c0a8934dda093ddc2
Jakub Nowak <86384726+ham222@users.noreply.github.com>
2023-05-10 15:14:33 +0200

    Update API calls to use .env vars
    Modified the MeasurementService.ts file to use the environment variable "REACT_APP_API_BASE_URL" instead of hard-coding the API URL into the file.

--- src/services/MeasurementService.ts
+++ src/services/MeasurementService.ts
@@ -1,11 +1,13 @@
 import axios from "axios";
 import Measurement from "src/domain/Measurement";
 
+const API_URL = process.env.REACT_APP_API_BASE_URL
+
 export async function getCurrentTemperature(): Promise<Measurement[]> {
   let temperatureList;
 
   try {
-    let url = `http://localhost:3100/api/temperature?current`;
+    let url = API_URL+`temperature?current`;
     const response = await axios.get(url);
     if (response.status !== 200) return [];
 
@@ -24,7 +26,7 @@ export async function getTemperature(
   let temperatureList;
 
   try {
-    let url = `http://localhost:3100/api/temperature`;
+    let url = API_URL+`temperature`;
 
     if (startTimestamp !== undefined) {
       url += `?startTimestamp=${startTimestamp}`;
@@ -52,7 +54,7 @@ export async function getCurrentHumidity(): Promise<Measurement[]> {
   let temperatureList;
 
   try {
-    let url = `http://localhost:3100/api/humidity?current`;
+    let url = API_URL+`humidity?current`;
     const response = await axios.get(url);
     if (response.status !== 200) return [];
 
@@ -70,7 +72,7 @@ export async function getHumidity(
 ): Promise<Measurement[]> {
   let temperatureList;
   try {
-    let url = `http://localhost:3100/api/humidity`;
+    let url = API_URL+`humidity`;
 
     if (startTimestamp !== undefined) {
       url += `?startTimestamp=${startTimestamp}`;
@@ -99,7 +101,7 @@ export async function getCurrentCo2(): Promise<Measurement[]> {
   let temperatureList;
 
   try {
-    let url = `http://localhost:3100/api/co2?current`;
+    let url = API_URL+`co2?current`;
     const response = await axios.get(url);
     if (response.status !== 200) return [];
 
@@ -117,7 +119,7 @@ export async function getCo2(
 ): Promise<Measurement[]> {
   let temperatureList;
   try {
-    let url = `http://localhost:3100/api/co2`;
+    let url = API_URL+`co2`;
     if (startTimestamp !== undefined) {
       url += `?startTimestamp=${startTimestamp}`;
     }
