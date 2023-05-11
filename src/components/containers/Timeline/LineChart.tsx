c8dc401537462d1279c0461a839425a8be6719e9
M0Rek <morek.090@gmail.com>
2023-05-11 16:15:01 +0200

    Add averaging measurements to line charts

--- src/components/containers/Timeline/LineChart.tsx
+++ src/components/containers/Timeline/LineChart.tsx
@@ -23,10 +23,24 @@ export default function LineChart({
   icon,
 }: LineChartProps) {
   const timeScopes = [
-    new TimeScope("day", Duration.fromObject({ days: 1 })),
-    new TimeScope("week", Duration.fromObject({ weeks: 1 })),
-    new TimeScope("month", Duration.fromObject({ months: 1 })),
-    new TimeScope("year", Duration.fromObject({ years: 1 })),
+    new TimeScope("last hour", Duration.fromObject({ hours: 1 })),
+    new TimeScope("6 hours", Duration.fromObject({ hours: 6 })),
+    new TimeScope("12 hours", Duration.fromObject({ hours: 12 })),
+    new TimeScope(
+      "day",
+      Duration.fromObject({ days: 1 }),
+      Duration.fromObject({ hours: 1 })
+    ),
+    new TimeScope(
+      "week",
+      Duration.fromObject({ weeks: 1 }),
+      Duration.fromObject({ hours: 6 })
+    ),
+    new TimeScope(
+      "month",
+      Duration.fromObject({ months: 1 }),
+      Duration.fromObject({ days: 1 })
+    ),
   ];
 
   const [timeScope, setTimeScope] = useState(timeScopes[0]);
@@ -35,12 +49,57 @@ export default function LineChart({
   if (measurements == null) {
     measurements = [];
   }
+
+  const averageMeasurements = (
+    measurements: Measurement[],
+    durationInMilliseconds: number
+  ): Measurement[] => {
+    const averagedData = [];
+    let sum: number = 0;
+    let count: number = 0;
+    let startTime: number = measurements[0].timestamp;
+
+    for (let i = 0; i < measurements.length; i++) {
+      const currentData = measurements[i];
+      sum += parseFloat(currentData.value as unknown as string);
+      count++;
+
+      const elapsedTime = currentData.timestamp - startTime;
+      if (elapsedTime >= durationInMilliseconds) {
+        const averageValue = sum / count;
+        const averagedMeasurement = new Measurement(
+          Math.round(averageValue * 100) / 100,
+          startTime
+        );
+        averagedData.push(averagedMeasurement);
+
+        // Reset variables for the next group
+        sum = 0;
+        count = 0;
+        startTime = currentData.timestamp;
+      }
+    }
+
+    return averagedData;
+  };
+
+  measurements = measurements.filter(
+    ({ timestamp }) => timestamp >= cutOffTimestamp
+  );
+
+  console.log(measurements);
+  if (timeScope.averageTo !== undefined) {
+    measurements = averageMeasurements(
+      measurements,
+      timeScope.averageTo.toMillis()
+    );
+  }
+  console.log(measurements);
+
   const series = [
     {
       name: capitalize(type),
-      data: measurements
-        .filter(({ timestamp }) => timestamp >= cutOffTimestamp)
-        .map(({ value }) => value),
+      data: measurements.map(({ value }) => value),
     },
   ];
 
--- src/domain/TimeScope.ts
+++ src/domain/TimeScope.ts
@@ -3,8 +3,14 @@ import { Duration } from "luxon";
 export default class TimeScope {
   name: string;
   scope: Duration;
-  constructor(name: string, scope: Duration) {
+  averageTo?: Duration;
+  constructor(
+    name: string,
+    scope: Duration,
+    averageTo: Duration | undefined = undefined
+  ) {
     this.name = name;
     this.scope = scope;
+    this.averageTo = averageTo;
   }
 }
