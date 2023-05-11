caa38d55f631b8d5f93a63686a86bb6d20c07fb0
ham222 <ham222@users.noreply.github.com>
2023-05-11 07:24:13 +0000

    Prettify code

--- src/services/MeasurementService.ts
+++ src/services/MeasurementService.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 import Measurement from "src/domain/Measurement";
 
-const API_URL = process.env.REACT_APP_API_BASE_URL
+const API_URL = process.env.REACT_APP_API_BASE_URL;
 
 export async function getCurrentTemperature(): Promise<Measurement[]> {
   let temperatureList;
 
   try {
-    let url = API_URL+`temperature?current`;
+    let url = API_URL + `temperature?current`;
     const response = await axios.get(url);
 
     temperatureList = response.data;
@@ -25,7 +25,7 @@ export async function getTemperature(
   let temperatureList;
 
   try {
-    let url = API_URL+`temperature`;
+    let url = API_URL + `temperature`;
 
     if (startTimestamp !== undefined) {
       url += `?startTimestamp=${startTimestamp}`;
@@ -52,7 +52,7 @@ export async function getCurrentHumidity(): Promise<Measurement[]> {
   let temperatureList;
 
   try {
-    let url = API_URL+`humidity?current`;
+    let url = API_URL + `humidity?current`;
     const response = await axios.get(url);
 
     temperatureList = response.data;
@@ -69,7 +69,7 @@ export async function getHumidity(
 ): Promise<Measurement[]> {
   let temperatureList;
   try {
-    let url = API_URL+`humidity`;
+    let url = API_URL + `humidity`;
 
     if (startTimestamp !== undefined) {
       url += `?startTimestamp=${startTimestamp}`;
@@ -96,7 +96,7 @@ export async function getCurrentCo2(): Promise<Measurement[]> {
   let temperatureList;
 
   try {
-    let url = API_URL+`co2?current`;
+    let url = API_URL + `co2?current`;
     const response = await axios.get(url);
 
     temperatureList = response.data;
@@ -113,7 +113,7 @@ export async function getCo2(
 ): Promise<Measurement[]> {
   let temperatureList;
   try {
-    let url = API_URL+`co2`;
+    let url = API_URL + `co2`;
     if (startTimestamp !== undefined) {
       url += `?startTimestamp=${startTimestamp}`;
     }
