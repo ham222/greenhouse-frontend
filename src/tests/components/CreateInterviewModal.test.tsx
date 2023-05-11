b67dfa39a4c9e11765934050a208823e3c6e2b6d
Jakub Nowak <86384726+ham222@users.noreply.github.com>
2023-05-11 10:24:04 +0200

    Fix default locale so tests are lang. independent

--- src/tests/components/CreateInterviewModal.test.tsx
+++ src/tests/components/CreateInterviewModal.test.tsx
@@ -1,7 +1,16 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
 import { render, screen } from "@testing-library/react";
 import CreateIntervalModal from "src/components/containers/Watering/CreateIntervalModal";
 import "@testing-library/jest-dom";
 
 describe("CreateIntervalModal", () => {
-    
+
+    const onClose = jest.fn()
+    const onAdd = jest.fn()
+
+    it("Renders without crashing", () => {
+        const root = ReactDOM.createRoot(document.createElement("div"))
+    root.render(<CreateIntervalModal open={true} onClose={onClose} onAdd={onAdd} />)
+    })
 })
