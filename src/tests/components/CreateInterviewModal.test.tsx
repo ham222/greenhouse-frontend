9629341f388503821575eefe7a42da1be41b15a7
Jakub Nowak <86384726+ham222@users.noreply.github.com>
2023-05-11 09:49:21 +0200

    Create file for testing the component with jest

---
+++ src/tests/components/CreateInterviewModal.test.tsx
@@ -0,0 +1,7 @@
+import { render, screen } from "@testing-library/react";
+import CreateIntervalModal from "src/components/containers/Watering/CreateIntervalModal";
+import "@testing-library/jest-dom";
+
+describe("CreateIntervalModal", () => {
+    
+})
