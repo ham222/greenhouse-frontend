0634d795e29964b5690eeaaf8b808a98d9e90d2f
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2023-05-11 15:07:51 +0200

    Add ToastContainer to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import Navbar from "./components/Navbar";
 import Status from "./components/containers/Status/Status";
 import Timeline from "./components/containers/Timeline/Timeline";
 import Watering from "./components/containers/Watering/Watering";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <>
       <div className="App">
         <div className="mb-24 md:mb-0 md:ml-[6rem] 2xl:ml-[8rem]">
+          <ToastContainer position="top-center" />
           <Routes>
             <Route path="/" element={<Status />} />
             <Route path="/timeline" element={<Timeline />} />
