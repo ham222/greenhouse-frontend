9089d0ba1306ef2dd78324b7a6e749c67de1d5d8
Jakub Nowak <86384726+ham222@users.noreply.github.com>
2023-05-11 12:49:10 +0200

    Fixed unit tests
    Made the tests async and awaited render in waitFor anonymous arrow function

--- src/tests/components/CreateInterviewModal.test.tsx
+++ src/tests/components/CreateInterviewModal.test.tsx
@@ -1,6 +1,7 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import {  fireEvent, render, screen, waitFor } from "@testing-library/react";
 import CreateIntervalModal from "src/components/containers/Watering/CreateIntervalModal";
 import "@testing-library/jest-dom";
+import {act} from 'react-dom/test-utils'
 
 class ResizeObserver {
     observe() {}
@@ -13,21 +14,27 @@ describe("CreateIntervalModal", () => {
     const onClose = jest.fn()
     const onAdd = jest.fn()
 
-    it("Renders without crashing", () => {
-        
-       render(<CreateIntervalModal open={true} onClose={onClose} onAdd={onAdd} />)
-    
+    it("Renders without crashing", async () => {
+        await waitFor(()=>{
+            render(<CreateIntervalModal open={true} onClose={onClose} onAdd={onAdd}  />)
+        })
     })
 
-    it("Displays turned off button", () => {
-        render(<CreateIntervalModal open={true} onClose={onClose} onAdd={onAdd} />)
+    it("Displays turned off button", async () => {
+        await waitFor(()=>{
+            render(<CreateIntervalModal open={true} onClose={onClose} onAdd={onAdd} />)
+        })
         const buttonToFind = screen.getByText('Confirm', { selector: 'button' })
         expect(buttonToFind).toBeDisabled()
     })
-    it("Makes the button clickable on setting day",()=>{
-        render(<CreateIntervalModal open={true} onClose={onClose} onAdd={onAdd} />)
+    it("Makes the button clickable on setting day",async ()=>{
+        await waitFor(()=>{
+            render(<CreateIntervalModal open={true} onClose={onClose} onAdd={onAdd} />)
+        })
         const dayButton = screen.getByTestId("Monday-switch")
-        fireEvent.click(dayButton)
+        act(()=>{
+            fireEvent.click(dayButton)
+        })
         const buttonToFind = screen.getByText('Confirm', { selector: 'button' })
         expect(buttonToFind).not.toBeDisabled()
     })
--- src/tests/components/Modal.test.tsx
+++ src/tests/components/Modal.test.tsx
@@ -1,26 +1,30 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import Modal from "src/components/UI/Modal";
 import "@testing-library/jest-dom";
 
 describe("Modal", () => {
   const onClose = jest.fn();
 
-  it("renders children when open prop is true", () => {
-    render(
-      <Modal title="Modal title" open={true} onClose={onClose}>
-        <div>Modal content</div>
-      </Modal>
-    );
+  it("renders children when open prop is true", async () => {
+    await waitFor(()=>{
+      render(
+        <Modal title="Modal title" open={true} onClose={onClose}>
+          <div>Modal content</div>
+        </Modal>
+      )
+    });
     expect(screen.getByText("Modal title")).toBeInTheDocument();
     expect(screen.getByText("Modal content")).toBeInTheDocument();
   });
 
-  it("does not render children when open prop is false", () => {
-    render(
-      <Modal title="Modal title" open={false} onClose={onClose}>
-        <div>Modal content</div>
-      </Modal>
-    );
+  it("does not render children when open prop is false", async () => {
+    await waitFor(()=>{
+      render(
+        <Modal title="Modal title" open={false} onClose={onClose}>
+          <div>Modal content</div>
+        </Modal>
+      )
+    });
 
     expect(screen.queryByText("Modal content")).not.toBeInTheDocument();
     expect(screen.queryByText("Modal title")).not.toBeInTheDocument();
