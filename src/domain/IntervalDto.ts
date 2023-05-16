1e672889a981513dc2dd682a92601720df62a00e
M0Rek <morek.090@gmail.com>
2023-05-16 19:03:01 +0200

    Change IntervalDto to class

--- src/domain/IntervalDto.ts
+++ src/domain/IntervalDto.ts
@@ -1,6 +1,20 @@
-export type IntervalDto = {
+import { DateTime } from "luxon";
+export default class IntervalDto {
   id: number;
   startTime: string;
   endTime: string;
   dayOfWeek: number;
-};
+  constructor(
+    id: number,
+    startTime: DateTime,
+    endTime: DateTime,
+    dayOfWeek: number
+  ) {
+    const format = "HH:mm:ss";
+
+    this.id = id;
+    this.startTime = startTime.toFormat(format);
+    this.endTime = endTime.toFormat(format);
+    this.dayOfWeek = dayOfWeek;
+  }
+}
