0c73a350b5d0776bb007d93c0b1ddad32a94eb5a
M0Rek <morek.090@gmail.com>
2023-05-18 12:17:13 +0200

    Add classname prop to textbutton

--- src/components/UI/TextButton.tsx
+++ src/components/UI/TextButton.tsx
@@ -3,13 +3,18 @@ import { ButtonUnstyled } from "@mui/base";
 interface TextButtonProps {
   onClick: () => void;
   text: string;
+  className?: string;
 }
 
-export default function TextButton({ onClick, text }: TextButtonProps) {
+export default function TextButton({
+  onClick,
+  text,
+  className,
+}: TextButtonProps) {
   return (
     <ButtonUnstyled
       onClick={() => onClick()}
-      className="p-2 w-full h-full rounded-lg bg-dark text-xs text-white hover:bg-dark-light duration-100 grid items-center"
+      className={`p-2 w-full h-full rounded-lg bg-dark text-xs text-white hover:bg-dark-light duration-100 grid items-center ${className}`}
     >
       {text}
     </ButtonUnstyled>
