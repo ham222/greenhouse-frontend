a2395f4b2a33cc711436f4e5d14fa2e3ba8dc9f9
Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
2023-05-18 14:57:15 +0200

    Update text size on different breakpoints

--- src/components/containers/Status/CurrentValBox.tsx
+++ src/components/containers/Status/CurrentValBox.tsx
@@ -12,13 +12,15 @@ let CurrentValBox = ({
 }: Props): JSX.Element => {
   return (
     <div className="bg-[#bffa00] font-sora shadow-lg max-sm:w-auto grid rounded-xl grid-cols-9 grid-rows-5">
-      <div className="col-span-4 row-span-3 border-b border-black border-dashed text-4xl font-bold flex justify-center items-center py-5">
+      <div className="col-span-4 row-span-3 border-b border-black border-dashed text-4xl md:text-5xl lg:text-4xl xl:text-5xl font-bold flex justify-center items-center py-5">
         {temperature}°C
       </div>
       <div className="col-span-5 row-span-3 border-b border-black border-dashed flex items-center justify-center">
         <div>
-          <div className="text-lg font-bold">Your Greenhouse</div>
-          <div className="text-sm">at {datetime}</div>
+          <div className="text-lg sm:text-xl xl:text-2xl font-bold">
+            Your Greenhouse
+          </div>
+          <div className="text-sm md:text-md xl:text-lg">at {datetime}</div>
         </div>
       </div>
       <div className="col-span-3 py-1 row-span-2 flex items-center justify-center">
