fedfb2e0b2988ed0ff6f88b2c9d8189b44c64816
M0Rek <morek.090@gmail.com>
2023-05-19 17:17:50 +0200

    Check for rendering in preset test
    Co-Authored-By: Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
    Co-Authored-By: ham222 <86384726+ham222@users.noreply.github.com>
    Co-Authored-By: Dávid Kocúr <62095094+Davidek776@users.noreply.github.com>

--- src/tests/components/Preset.test.tsx
+++ src/tests/components/Preset.test.tsx
@@ -1,55 +1,57 @@
-import React from "react";
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
-import axios from "axios";
-import Preset from "src/components/containers/Presets/Preset";
+import { render } from "@testing-library/react";
+import Preset from "src/components/containers/Presets/Presets";
 
 jest.mock("axios");
 
-// describe("Preset", () => {
-//   beforeEach(() => {
-//     jest.resetAllMocks();
-//   });
+describe("Preset", () => {
+  it("renders without crashing", () => {
+    render(<Preset />);
+  });
 
-//   test("renders preset name input", () => {
-//     render(<Preset />);
-//     const presetNameInput = screen.getByTestId(
-//       "name-input"
-//     ) as HTMLInputElement;
-//     expect(presetNameInput).toBeInTheDocument();
-//   });
+  //   beforeEach(() => {
+  //     jest.resetAllMocks();
+  //   });
 
-//   test("updates preset name input value", () => {
-//     render(<Preset />);
-//     const presetNameInput = screen.getByTestId(
-//       "name-input"
-//     ) as HTMLInputElement;
+  //   test("renders preset name input", () => {
+  //     render(<Preset />);
+  //     const presetNameInput = screen.getByTestId(
+  //       "name-input"
+  //     ) as HTMLInputElement;
+  //     expect(presetNameInput).toBeInTheDocument();
+  //   });
 
-//     fireEvent.change(presetNameInput, { target: { value: "Test Preset" } });
-//     expect(presetNameInput.value).toBe("Test Preset");
-//   });
+  //   test("updates preset name input value", () => {
+  //     render(<Preset />);
+  //     const presetNameInput = screen.getByTestId(
+  //       "name-input"
+  //     ) as HTMLInputElement;
 
-//   test("calls addPreset and fetchData when save button is clicked", async () => {
-//     const mockPost = axios.post as jest.Mock;
-//     const mockGet = axios.get as jest.Mock;
+  //     fireEvent.change(presetNameInput, { target: { value: "Test Preset" } });
+  //     expect(presetNameInput.value).toBe("Test Preset");
+  //   });
 
-//     mockPost.mockResolvedValueOnce({ data: {} });
-//     mockGet.mockResolvedValueOnce({ data: [] });
+  //   test("calls addPreset and fetchData when save button is clicked", async () => {
+  //     const mockPost = axios.post as jest.Mock;
+  //     const mockGet = axios.get as jest.Mock;
 
-//     render(<Preset />);
-//     const saveButton = screen.getByText("Save");
-//     fireEvent.click(saveButton);
+  //     mockPost.mockResolvedValueOnce({ data: {} });
+  //     mockGet.mockResolvedValueOnce({ data: [] });
 
-//     await waitFor(() => {
-//       expect(mockPost).toHaveBeenCalledTimes(1);
-//       expect(mockGet).toHaveBeenCalledTimes(1);
-//     });
-//   });
+  //     render(<Preset />);
+  //     const saveButton = screen.getByText("Save");
+  //     fireEvent.click(saveButton);
 
-//   test('renders all presets modal when "See all presets" button is clicked', () => {
-//     render(<Preset />);
-//     const seeAllPresetsButton = screen.getByText("See all presets");
-//     fireEvent.click(seeAllPresetsButton);
-//     const modal = screen.getByTestId("all-presets-modal");
-//     expect(modal).toBeInTheDocument();
-//   });
-// });
+  //     await waitFor(() => {
+  //       expect(mockPost).toHaveBeenCalledTimes(1);
+  //       expect(mockGet).toHaveBeenCalledTimes(1);
+  //     });
+  //   });
+
+  //   test('renders all presets modal when "See all presets" button is clicked', () => {
+  //     render(<Preset />);
+  //     const seeAllPresetsButton = screen.getByText("See all presets");
+  //     fireEvent.click(seeAllPresetsButton);
+  //     const modal = screen.getByTestId("all-presets-modal");
+  //     expect(modal).toBeInTheDocument();
+  //   });
+});
