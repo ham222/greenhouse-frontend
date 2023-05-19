dd17a36cef5ea00d66a5ae28b5c61eae7c789fca
M0Rek <morek.090@gmail.com>
2023-05-19 17:16:34 +0200

    Remove unnecessary title in smallscreenmodal
    Co-Authored-By: Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
    Co-Authored-By: ham222 <86384726+ham222@users.noreply.github.com>
    Co-Authored-By: Dávid Kocúr <62095094+Davidek776@users.noreply.github.com>

--- src/components/UI/ModalSmallScreen.tsx
+++ src/components/UI/ModalSmallScreen.tsx
@@ -26,11 +26,11 @@ export default function ModallgallScreen({
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          <div className="fixed inset-0 lg:hidden bg-gray-500 bg-opacity-75 transition-opacity" />
         </Transition.Child>
 
-        <div className="fixed inset-0 z-10 overflow-y-auto ">
-          <div className="flex min-h-full items-start justify-center text-center lg:items-center lg:p-0">
+        <div className="fixed inset-0 z-10 overflow-y-auto">
+          <div className="flex items-start justify-center text-center lg:items-center lg:p-0">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
@@ -40,14 +40,8 @@ export default function ModallgallScreen({
               leaveFrom="opacity-100 translate-y-0 lg:scale-100"
               leaveTo="opacity-0 translate-y-4 lg:translate-y-0 lg:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden max-lg:h-screen lg:rounded-lg bg-white text-left shadow-xl transition-all lg:my-8 w-full lg:max-w-lg">
-                <Dialog.Title
-                  as="h3"
-                  className="text-xl mt-4 text-center font-semibold leading-6 text-gray-900"
-                >
-                  {title}
-                </Dialog.Title>
-                <div className="h-full grid ">
+              <Dialog.Panel className="relative lg:hidden overflow-scroll transform max-lg:h-screen lg:rounded-lg bg-white text-left shadow-xl transition-all lg:my-8 w-full lg:max-w-lg">
+                <div className=" grid">
                   <div>{children}</div>
                 </div>
               </Dialog.Panel>
