c51ad158670f66b9cbeba037b2e560c46089503c
M0Rek <morek.090@gmail.com>
2023-05-19 17:19:12 +0200

    Add decimal support and fix bugs
    Co-Authored-By: Aivaraszev <36788893+Aivaraszev@users.noreply.github.com>
    Co-Authored-By: ham222 <86384726+ham222@users.noreply.github.com>
    Co-Authored-By: Dávid Kocúr <62095094+Davidek776@users.noreply.github.com>

--- src/components/containers/Presets/ThresholdBox.tsx
+++ src/components/containers/Presets/ThresholdBox.tsx
@@ -16,11 +16,13 @@ let ThresholdBox = ({
   let limit: Limit;
 
   if (limitMap.has(threshold.type)) {
-    limit = limitMap.get(threshold.type) ?? { min: -9999, max: 9999 };
+    limit = limitMap.get(threshold.type) ?? { min: -9999, max: 9999, round: 0 };
   }
 
   const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
-    const value = parseFloat(evt.target.value);
+    const value =
+      Math.round(parseFloat(evt.target.value) * Math.pow(10, limit.round)) /
+      Math.pow(10, limit.round);
 
     const newThreshold = new Threshold(
       threshold.type,
@@ -33,48 +35,60 @@ let ThresholdBox = ({
         limit.min,
         Math.min(limit.max, Number(value))
       );
-
-      updateValue(newThreshold);
     } else {
       newThreshold.min = Math.max(
         limit.min,
         Math.min(limit.max, Number(value))
       );
-
-      updateValue(newThreshold);
     }
+
+    updateValue(newThreshold);
   };
   return (
     <>
-      <div className="flex flex-col lg:flex-row my-5 ">
-        <h2 className="text-center font-semibold text-lg mb-4 lg:mb-0 lg:w-28 lg:text-right">
+      <div className="flex lg:justify-center lg:items-center flex-col my-5 ">
+        <div className="text-center justify-center font-semibold text-lg mb-4 lg:text-right">
           {capitalize(threshold.type)}
-        </h2>
-        <div className="grid grid-cols-2 mx-4 lg:gap-10">
-          <div className="flex justify-start gap-3 items-center ">
-            <p className="text-lg">Min</p>
-            <div className=" ">
-              <input
-                type="number"
-                name="min"
-                id=""
-                className="py-1 w-full bg-[#EFEFEF] rounded-lg"
-                onChange={handleChange}
-                value={Number.isNaN(threshold.min) ? "" : threshold.min}
-              />
+        </div>
+        <div className="grid gap-2 grid-cols-2 mx-4 lg:gap-10">
+          <div className="flex justify-center gap-3 items-center ">
+            <div>
+              <label
+                htmlFor={`min-${threshold.type}`}
+                className="block text-sm font-medium leading-3"
+              >
+                Min
+              </label>
+              <div className="mt-2">
+                <input
+                  id={`min-${threshold.type}`}
+                  name="min"
+                  type="number"
+                  onChange={handleChange}
+                  value={Number.isNaN(threshold.min) ? "" : threshold.min}
+                  className="block pl-3 w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-neon sm:text-sm sm:leading-6"
+                />
+              </div>
             </div>
           </div>
-          <div className="flex justify-end gap-3 items-center">
-            <p className="text-lg">Max</p>
-            <div className=" ">
-              <input
-                type="number"
-                name="max"
-                id=""
-                className="py-1 w-full bg-[#EFEFEF]  rounded-lg "
-                onChange={handleChange}
-                value={Number.isNaN(threshold.max) ? "" : threshold.max}
-              />
+          <div className="flex justify-center gap-3 items-center">
+            <div>
+              <label
+                htmlFor={`max-${threshold.type}`}
+                className="block text-sm font-medium leading-3"
+              >
+                Max
+              </label>
+              <div className="mt-2">
+                <input
+                  id={`max-${threshold.type}`}
+                  name="max"
+                  type="number"
+                  onChange={handleChange}
+                  value={Number.isNaN(threshold.max) ? "" : threshold.max}
+                  className="block pl-3 w-full rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-neon sm:text-sm sm:leading-6"
+                />
+              </div>
             </div>
           </div>
         </div>
