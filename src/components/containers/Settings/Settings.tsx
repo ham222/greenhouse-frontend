86ac32c6bc105c4b788f882fea3dc537fd5bdad0
Jakub Nowak <86384726+ham222@users.noreply.github.com>
2023-05-25 13:42:27 +0200

    Make update email responsive

--- src/components/containers/Settings/Settings.tsx
+++ src/components/containers/Settings/Settings.tsx
@@ -40,30 +40,31 @@ export default function Settings() {
   return (
     <>
       <div className="m-2 mt-4">
-        <div className="mt-2 flex justify-around">
-          <div className="">
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium leading-3"
+        <div className="flex justify-center md:w-full">
+          <div className="flex gap-2 max-md:w-96 flex-col md:flex-row justify-evenly items-center md:grow">
+            <div className="md:w-72 w-full">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-3"
+              >
+                E-mail:
+              </label>
+              <input
+                id="email"
+                name="email"
+                type="text"
+                value={emailResponse.data?.email ?? ""}
+                className="mt-2 w-full block pl-3 text-stone-600 rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-neon sm:text-sm sm:leading-6"
+                disabled
+              />
+            </div>
+            <button
+              className="md:w-auto bg-white w-full text-red-600 py-4 rounded-xl px-2 border-2 border-red-600 ease-in-out duration-300 hover:shadow-xl"
+              onClick={() => openEmailModal()}
             >
-              E-mail:
-            </label>
-            <input
-              id="email"
-              name="email"
-              type="text"
-              value={emailResponse.data?.email ?? ""}
-              className="mt-2 block pl-3 text-stone-600 rounded-md border-0 py-1.5  shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-neon sm:text-sm sm:leading-6"
-              disabled
-            />
+              Change e-mail
+            </button>
           </div>
-
-          <button
-            className="bg-white text-red-600 py-4 rounded-xl px-2 border-2 border-red-600 ease-in-out duration-300 hover:shadow-xl"
-            onClick={() => openEmailModal()}
-          >
-            Change e-mail
-          </button>
         </div>
       </div>
       <ChangeEmailModal
