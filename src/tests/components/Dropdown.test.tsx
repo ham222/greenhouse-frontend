4721539b55f62913646744a5f71ba91c8cb1d6b8
M0Rek <morek.090@gmail.com>
2023-05-27 21:18:43 +0200

    Update drop down tests

--- src/tests/components/Dropdown.test.tsx
+++ src/tests/components/Dropdown.test.tsx
@@ -5,13 +5,13 @@ describe("Dropdown", () => {
   it("renders correctly", () => {
     render(
       <Dropdown
-        title={"Test Title"}
+        selected={"title"}
         onSelect={(option) => {}}
         options={["option 0", "option 1", "option 2"]}
       />
     );
 
-    expect(screen.getByText("Test Title")).toBeInTheDocument();
+    expect(screen.getByText("title")).toBeInTheDocument();
     expect(screen.queryByText("option 0")).not.toBeInTheDocument();
     expect(screen.queryByText("option 1")).not.toBeInTheDocument();
     expect(screen.queryByText("option 2")).not.toBeInTheDocument();
@@ -20,15 +20,15 @@ describe("Dropdown", () => {
   it("renders correctly when opened", () => {
     render(
       <Dropdown
-        title={"Test Title"}
+        selected={"title"}
         onSelect={(option) => {}}
         options={["option 0", "option 1", "option 2"]}
       />
     );
 
-    fireEvent.click(screen.getByText("Test Title"));
+    fireEvent.click(screen.getByText("title"));
 
-    expect(screen.getByText("Test Title")).toBeInTheDocument();
+    expect(screen.getByText("title")).toBeInTheDocument();
     expect(screen.getByText("option 0")).toBeInTheDocument();
     expect(screen.getByText("option 1")).toBeInTheDocument();
     expect(screen.getByText("option 2")).toBeInTheDocument();
@@ -39,13 +39,13 @@ describe("Dropdown", () => {
 
     render(
       <Dropdown
-        title={"Test Title"}
+        selected={"title"}
         onSelect={mockFn}
         options={["option 0", "option 1", "option 2"]}
       />
     );
 
-    fireEvent.click(screen.getByText("Test Title"));
+    fireEvent.click(screen.getByText("title"));
     fireEvent.click(screen.getByText("option 1"));
     expect(mockFn).lastCalledWith("option 1");
   });
@@ -53,16 +53,16 @@ describe("Dropdown", () => {
   it("renders correctly when opened and closed", () => {
     render(
       <Dropdown
-        title={"Test Title"}
+        selected={"title"}
         onSelect={(option) => {}}
         options={["option 0", "option 1", "option 2"]}
       />
     );
 
-    fireEvent.click(screen.getByText("Test Title"));
-    fireEvent.click(screen.getByText("Test Title"));
+    fireEvent.click(screen.getByText("title"));
+    fireEvent.click(screen.getByText("title"));
 
-    expect(screen.getByText("Test Title")).toBeInTheDocument();
+    expect(screen.getByText("title")).toBeInTheDocument();
     expect(screen.queryByText("option 0")).not.toBeInTheDocument();
     expect(screen.queryByText("option 1")).not.toBeInTheDocument();
     expect(screen.queryByText("option 2")).not.toBeInTheDocument();
