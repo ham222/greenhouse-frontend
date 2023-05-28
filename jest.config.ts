b2596d0e47cb4962e3b2519f30059e9b7a224531
M0Rek <morek.090@gmail.com>
2023-05-28 22:54:58 +0200

    Modify jest config

--- jest.config.ts
+++ jest.config.ts
@@ -71,14 +71,7 @@ const config = {
   moduleDirectories: ["node_modules", "<rootDir>/src"],
 
   // An array of file extensions your modules use
-  // moduleFileExtensions: [
-  //   "js",
-  //   "jsx",
-  //   "ts",
-  //   "tsx",
-  //   "json",
-  //   "node"
-  // ],
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
