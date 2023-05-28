2f9dd4b908324a78ac48edb4f9c4797b6b26da50
M0Rek <M0Rek@users.noreply.github.com>
2023-05-28 20:59:09 +0000

    Prettify code

--- src/tests/components/validatePreset.test.tsx
+++ src/tests/components/validatePreset.test.tsx
@@ -1,25 +1,25 @@
-import Preset from 'src/domain/Preset';
-import Threshold from 'src/domain/Threshold';
-import ValidatePreset from 'src/utils/validatePreset';
+import Preset from "src/domain/Preset";
+import Threshold from "src/domain/Threshold";
+import ValidatePreset from "src/utils/validatePreset";
 
-describe('Preset Validation', () => {
-  it('should return false if preset name is empty', () => {
-    const preset = new Preset('', [new Threshold("type1", 0, 100)]);
+describe("Preset Validation", () => {
+  it("should return false if preset name is empty", () => {
+    const preset = new Preset("", [new Threshold("type1", 0, 100)]);
     expect(ValidatePreset(preset)).toBe(false);
   });
 
-  it('should return false if thresholds min or max are NaN', () => {
-    const preset = new Preset('Preset1', [new Threshold("type1", NaN, 100)]);
+  it("should return false if thresholds min or max are NaN", () => {
+    const preset = new Preset("Preset1", [new Threshold("type1", NaN, 100)]);
     expect(ValidatePreset(preset)).toBe(false);
   });
 
-  it('should return false if thresholds min is larger than max', () => {
-    const preset = new Preset('Preset1', [new Threshold("type1", 100, 0)]);
+  it("should return false if thresholds min is larger than max", () => {
+    const preset = new Preset("Preset1", [new Threshold("type1", 100, 0)]);
     expect(ValidatePreset(preset)).toBe(false);
   });
 
-  it('should return true if preset and thresholds are valid', () => {
-    const preset = new Preset('Preset1', [new Threshold("type1", 0, 100)]);
+  it("should return true if preset and thresholds are valid", () => {
+    const preset = new Preset("Preset1", [new Threshold("type1", 0, 100)]);
     expect(ValidatePreset(preset)).toBe(true);
   });
 });
