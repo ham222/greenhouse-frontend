570ba512676565bd38d7f52fb8019451c6a0669c
M0Rek <morek.090@gmail.com>
2023-05-29 15:24:38 +0200

    Smoke Test Presets

--- src/tests/components/Presets.test.tsx
+++ src/tests/components/Presets.test.tsx
@@ -1,9 +1,34 @@
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import axios, { AxiosResponse } from "axios";
 import { HashRouter } from "react-router-dom";
 import Presets from "src/components/containers/Presets/Presets";
+import Preset from "src/domain/Preset";
+import Threshold from "src/domain/Threshold";
 
 describe("Presets", () => {
   it("renders", () => {
+    render(
+      <HashRouter>
+        <Presets />
+      </HashRouter>
+    );
+  });
+
+  it("renders on small screens", () => {
+    Object.defineProperty(window, "innerWidth", {
+      configurable: true,
+      value: 400,
+      writable: true,
+    });
+
+    render(
+      <HashRouter>
+        <Presets />
+      </HashRouter>
+    );
+  });
+
+  it("renders without crashing", async () => {
     render(
       <HashRouter>
         <Presets />
